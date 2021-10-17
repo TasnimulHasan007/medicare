7cb75d80b6cbd6f702fb42b2d79d274f40fdd122
Tasnimul Hasan <thtanim2005@gmail.com>
2021-10-18 00:13:24 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+import Button from "react-bootstrap/Button"
+import "./App.css"
+
+function App() {
+  return (
+    <Container>
+      <Button variant="primary">Hello world</Button>
+    </Container>
+  )
+}
+
+export default App
